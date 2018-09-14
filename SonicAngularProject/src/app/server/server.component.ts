2db6cd71ab280ac52a699d59aed0f46928ac1d08
namjoshik <namjoshi20@live.com>
2018-09-14 08:11:44 +0530

    Added server component

---
+++ SonicAngularProject/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// @ts-ignore
+// @ts-ignore
+@Component ({
+  selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
